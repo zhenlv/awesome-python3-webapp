e8c211ae00de2d28aea002f5bf7e8b00aa051a67
lvzhen <sdhtlz@126.com>
2018-09-08 17:35:29 +0800

    add REST API

--- www/handlers.py
+++ www/handlers.py
@@ -14,5 +14,18 @@ async def index(request):
 	return {
 		'__template__':'blogs.html',
 		'blogs':blogs
-	}
+	}	
+
+@get('/api/users')
+async def api_get_users(*,page='1'):
+	page_index = get_page_index(page)
+	num = await User.findNumber('count(id)')
+	p = Page(num,page_index)
+	if num == 0 :
+		return dict(page=p,users=())
+	users = yield from User.findAll(orderby='created_at desc',limit=(p.offset,p.limit))
+	for u in users:
+		u.passwd = '******'
+	return dict(page=p,users=users)
+
 
